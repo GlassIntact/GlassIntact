25c29795cb85dcd2ea3e43a8dedbd0aa908525cc
Kristen Duong <kkristene3@gmail.com>
2021-10-26 02:03:50 -0400

    Working on scrolling effect

--- Product Description/ProductDescription.js
+++ Product Description/ProductDescription.js
@@ -24,3 +24,23 @@ window.addEventListener("DOMContentLoaded", ()=>{
         }, 2300);
     })
 })
+
+/* Scroll Effect */
+window.addEventListener("scroll", reveal);
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+
+        var windowHeight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect().top;
+        var revealPoint = 150;
+
+        if (revealTop < (windowHeight - revealPoint)) {
+            reveals[i].classList.add("active");
+        }
+        else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
