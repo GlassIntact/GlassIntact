7a509ec2e1ec9acf1df6322a2ef46a4cca68ded9
Samuel Braun <samuelbraun17@gmail.com>
2021-11-06 23:22:09 -0400

    Removed lots of unnecessary comments & obsolete code

--- Meet The Team/Meet The Team.js
+++ Meet The Team/Meet The Team.js
@@ -1,15 +1,11 @@
 $(document).ready(function(){
     $("h5").hide()
     $("p5").hide()
-    //$("p5").hide()
-  //  $("h3").hide()
     $('div.myEverything').hide()
     $('div')
 
 
     $("h5").fadeIn(2000)
     $("p5").fadeIn(3000)
-    //$("p5").fadeIn(2000)
-    //$("h3").fadeIn(2000)
     $('div.myEverything').fadeIn(4000)
   });
